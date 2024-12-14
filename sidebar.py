564e99fe6035c0c5543618f86efe743c84b7f2da
papirozy <papirozy@gmail.com>
2024-12-14 18:56:52 +0000

    reversed the path changes

--- sidebar.py
+++ sidebar.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from api.api_utils import upload_document, list_documents, delete_document
+from api_utils import upload_document, list_documents, delete_document
 
 def display_sidebar():
     # model Selection
